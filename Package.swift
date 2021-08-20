e57990e4de7c2fc84c66bd1ea67b8123173d29f2
marcinpodeszwazen <marcin.podeszwa@zen.com>
2021-08-20 13:29:53 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+  name: "ZENCheckout",
+  platforms: [
+    .iOS(.v11)
+  ],
+  products: [
+  ],
+  dependencies: [
+  ],
+  targets: [
+  ]
+)}
