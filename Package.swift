ce7dd0ca293eed2cc4445146a40dca5cc22bac4a
marcinpodeszwazen <marcin.podeszwa@zen.com>
2021-08-20 16:07:36 +0200

    Release 0.0.1

--- Package.swift
+++ Package.swift
@@ -4,12 +4,30 @@ import PackageDescription
 let package = Package(
   name: "ZENCheckout",
   platforms: [
-    .iOS(.v11)
+    .iOS(.v13)
   ],
   products: [
+    .library(
+      name: "ZENCheckoutAPI",
+      targets: ["ZENCheckoutAPI"]
+    ),
+    .library(
+      name: "ZENCheckout",
+      targets: ["ZENCheckout"]
+    ),
   ],
   dependencies: [
   ],
   targets: [
+    .binaryTarget(
+      name: "ZENCheckoutAPI",
+      url: "https://github.com/marcinpodeszwazen/testspm/releases/download/0.0.1/ZENCheckoutAPI.xcframework.zip",
+      checksum: "02f688848be2bc4ed6d869e2d5378ede85d4058efce9bc0ad97da87a7990a435"
+    ),
+    .binaryTarget(
+      name: "ZENCheckout",
+      url: "https://github.com/marcinpodeszwazen/testspm/releases/download/0.0.1/ZENCheckout.xcframework.zip",
+      checksum: "4b2ae42e2d86e3c0a39a6f967897f55cdb8e164eccf04f17b935483cc08ec816"
+    ),
   ]
 )
